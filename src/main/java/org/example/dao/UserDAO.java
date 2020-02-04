dc8947a59beacf0fe31184746dc35da4b0269646
deepak-21-12 <deepak211293@gmail.com>
2020-02-04 17:44:34 +0530

    Added the list in UserDAO

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -4,12 +4,13 @@ import org.example.model.User;
 import org.hibernate.Session;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface UserDAO {
     // get all users
     // get user by name
     // get user by id
-    public void getAllUsers(Session crsession);
+    public List<User> getAllUsers(Session crsession);
     public void getUserByname();
     public void getId();
     void save(User user);
